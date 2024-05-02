735e3b69a78d557d8165c391009a0432f4722d52
SergioMerinoCortezdev <123787160+SergioMerinoCortezdev@users.noreply.github.com>
2024-05-01 20:50:57 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,7 @@
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('.portafolio',{delay:250});
+ScrollReveal().reveal('.card1',{delay:500});
+ScrollReveal().reveal('.card2',{delay:600});
+ScrollReveal().reveal('.card3',{delay:700});
+ScrollReveal().reveal('.lenguajes',{delay:200});
+ScrollReveal().reveal('.footer',{delay:500});
