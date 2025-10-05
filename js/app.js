5d2a3805d57aca0044d6932800752b950b62fa27
SergioMerinoCortezdev <123787160+SergioMerinoCortezdev@users.noreply.github.com>
2025-10-05 14:49:20 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,3 +5,24 @@ ScrollReveal().reveal('.card2',{delay:600});
 ScrollReveal().reveal('.card3',{delay:700});
 ScrollReveal().reveal('.lenguajes',{delay:200});
 ScrollReveal().reveal('.footer',{delay:500});
+
+  const toggle = document.getElementById("nav-toggle");
+  const menu = document.getElementById("nav-menu");
+
+  toggle.addEventListener("click", () => {
+    toggle.classList.toggle("active");
+    menu.classList.toggle("show");
+  });
+
+  // Cerrar menú al hacer clic en enlace
+  document.querySelectorAll(".nav__links a").forEach(link => {
+    link.addEventListener("click", () => {
+      menu.classList.remove("show");
+      toggle.classList.remove("active");
+    });
+  });
+
+
+
+ 
+
